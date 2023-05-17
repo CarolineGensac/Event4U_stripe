324532f95059d2c6e72e4bbfc9cecad5172883ce
caroline <gensac.caroline@gmail.com>
2023-05-17 19:41:43 +0200

    first

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user
+  before_action :authenticate_administrator
+
+  def show
+    @user = User.find(params[:format])
+  end
+
+private
+
+  def authenticate_user
+    unless current_user
+      flash[:alert] = "Merci de vous connecter !"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def authenticate_administrator
+    unless current_user == User.find(params[:format])
+      flash[:alert] = "Vous ne pouvez accéder qu'à votre profil"
+      redirect_to root_path
+    end
+  end
+end
+# def show
+  #   @user = User.find(params[:format]) #peut être qu'il faudra remettre id un jour !!!!!
+  # end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  root to: "events#index"
+
+  resources :attendances
+  resources :events
+  get '/users', to: 'users#show'
+
+  devise_for :users
+
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+end
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
